9335198bc37e73fd94d5f4cced750f394d4580d2
Michael Campbell <michael@macpczone.co.uk>
2017-09-12 20:23:42 +0100

    Moved gdb and libconfig back out of disabled for the merge

---
+++ packages/libconfig/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=http://www.hyperrealm.com/libconfig/
+TERMUX_PKG_DESCRIPTION="Library for processing structured configuration files"
+TERMUX_PKG_VERSION=1.5
+TERMUX_PKG_SRCURL=https://github.com/hyperrealm/libconfig/archive/v1.5.tar.gz
+TERMUX_PKG_SHA256=cae5c02361d8a9b2bb26946c64f089d2e5e599972f386203fbc48975c0d885c8
+TERMUX_PKG_FOLDERNAME=libconfig-${TERMUX_PKG_VERSION}
